1a58b0a42f5a23456024d49e2a2bc07d96293455
devlearn555 <email.amankashyap@gmail.com>
2023-06-15 10:53:27 +0530

    variablesScopes in JS

---
+++ scopeVariables.js
@@ -0,0 +1,117 @@
+var a = 25;
+
+function abc(){
+    console.log(a, "25")
+     a =50;
+    console.log(a, "50")
+    a = 90
+    console.log(a, "error")
+    return true;
+}
+console.log(a, "25")
+abc() 
+console.log(abc(), "50")
+
+// let b = 78;
+// console.log(b)
+
+// function abd(){
+//     console.log(b)
+//      b =50;
+//     console.log(b)
+// }
+// abd();
+
+// const d = 78;
+
+// console.log(d)
+// function abe(){
+//     console.log(d)
+//      d =50;
+//     console.log(d)
+// }
+// abe();
+// let test = 25;
+// function abe(){
+//     // console.log(test, "80") 
+//     const test =80;
+//     console.log(test, "again 80") 
+//     test = 90;
+//     console.log(test, "90 again")
+
+// }
+// test = 100;
+// console.log(test, "error11111111")
+// console.log(test, "25 first")
+// abe();
+
+
+
+
+
+
+var a = 500;
+
+{
+     var a = 900;
+      a = 10000;
+    console.log(a, "100000")
+}
+
+console.log(a, "1000")
+
+// var a = "venkatesh"
+
+// {
+//     var a = "hcm-lead-developer"
+//     console.log(a)
+// }
+// console.log(a)
+
+// const a = 90000;
+
+// {
+//     const a = 600;
+//          a = 800000;
+//     console.log(a)
+// }
+// console.log(a)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var know = "venky"
+
+// {
+//     let know = "ooooooooo"
+//     // var know = "pppppppppp" 
+//     console.log(know, "oooooooooo")
+// }
+
+// console.log(know, "venky")
