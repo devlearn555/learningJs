641b18b004850633a97e7e78173f6ec6ce213979
devlearn555 <136675140+devlearn555@users.noreply.github.com>
2023-06-15 11:00:41 +0530

    Add files via upload

---
+++ higherOrderFunctions.js
@@ -0,0 +1,25 @@
+// higher order functions in js
+
+
+function xyz() {
+    console.log('lower order function')
+
+}
+function abc(xyz) {
+    console.log('higher order function')
+    xyz()
+}
+
+abc(xyz)
+
+//1.'higer order function' 2.lower order function 
+
+// to do--
+
+/*
+Map
+filter
+packageunderscore
+array 
+objects
+*/
